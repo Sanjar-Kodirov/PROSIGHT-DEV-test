6ec3e29a067755cbbb41e89887a6a4350d85dcbd
Sanjar  Kodirov <kodirov.sanzhar@bk.ru>
2025-02-17 11:55:23 +0500

    feat: add database configuration and Swagger setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'hh-pgsql-public.ebi.ac.uk',
+      port: 5432,
+      username: 'reader',
+      password: 'NWDMCE5xdipIjRrp',
+      database: 'pfmegrnargs',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      logging: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Locus API')
+    .setDescription(
+      'API for accessing locus data with filtering and sideloading capabilities',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
