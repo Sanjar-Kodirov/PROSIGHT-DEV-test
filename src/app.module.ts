6f4293f4cb460100965144e8181c7915018c1ba6
Sanjar Kodirov <kodirov.sanzhat@bk.ru>
2025-02-17 15:12:17 +0500

    chore: env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocusModule } from './locus/locus.module';
 import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'hh-pgsql-public.ebi.ac.uk',
-      port: 5432,
-      username: 'reader',
-      password: 'NWDMCE5xdipIjRrp',
-      database: 'pfmegrnargs',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false,
     }),
